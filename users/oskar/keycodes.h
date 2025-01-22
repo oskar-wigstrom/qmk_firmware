8c7f6207a6f01bb9134b13a9df50014dc01ccf29
Oskar Wigström <oskar.wigstrom@gmail.com>
2025-01-22 21:11:33 +0100

    ...

--- users/oskar/keycodes.h
+++ users/oskar/keycodes.h
@@ -8,18 +8,7 @@
 #include "quantum/quantum_keycodes.h"
 #include "oneshot.h"
 
-enum layers {
-    _BASE = 0,
-    _SWE,
-    _NUM,
-    _NAV,
-    _WNAV,
-    _SYM,
-    _MODS,
-    _SHRT,
-    _FUN,
-    _SPEC,
-};
+enum layers { _BASE = 0, _SWE, _NUM, _NAV, _WNAV, _SYM, _MODS, _FUN };
 
 enum custom_keycodes {
     // Direct dead keys ~`^
@@ -61,26 +50,28 @@ enum custom_keycodes {
     // Repeat keys
     REPEAT,
     REV_REP,
+
+    //
+    AT_E,
+    QE
 };
 
 #define xxxxxxx KC_NO
 
 #define TG_NAV TG(_NAV)
 #define TO_BASE TO(_BASE)
-#define R_THMB OS_SHFT
 
-#define MT_SPC LT(_NAV, KC_SPC)
+#define L_THMB_L LT(_NAV, KC_SPC)
+#define L_THMB_R KC_TAB
+#define R_THMB_L OS_SHFT
+#define R_THMB_R KC_ENT
+
 #define MY_RALT OSM(MOD_RALT)
-#define DN_CTRL LCTL_T(KC_DOWN)
 
 #define ESC_SYM LT(_SYM, KC_ESC)
-#define TAB_MOD LT(_MODS, KC_TAB)
-
 #define COLN_SYM LT(_SYM, SE_COLN)
 
-#define C_TAB C(KC_TAB)
-#define S_TAB S(KC_TAB)
-#define SC_TAB S(C(KC_TAB))
+#define TAB_MOD LT(_MODS, KC_TAB)
 
 #define MY_LALT OSM(MOD_LALT)
 
@@ -90,12 +81,6 @@ enum custom_keycodes {
 #define SPEC OSL(_SPEC)
 #define FUN OSL(_FUN)
 
-#define GAME2 OSL(_GAME2)
-
-#define SYM_LFT ALGR(SE_Y)    // ← y
-#define SYM_DWN ALGR(SE_U)    // ↓ u
-#define SYM_RHT ALGR(SE_I)    // → i
-#define SYM_UP ALGR(S(SE_U))  // ↑ U
 #define SYM_LDQ ALGR(SE_V)    // “ v
 #define SYM_RDQ ALGR(SE_B)    // ” b
 #define SYM_LQO ALGR(S(SE_V)) // ‘ V
